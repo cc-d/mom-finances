2a092deae28202c33aeeb89005c72425d307dea1
Cary Carter <ccarterdev@gmail.com>
2022-08-08 20:07:58 -0500

    too tired to finish tonight tome 2 sleep

--- calc.py
+++ calc.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 import csv
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import re
 import sys
+import click
+import logging
 
-rootd = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = '{}/sqlite.db'
-db = SQLAlchemy(app)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+rootd, app, db = '', None, None
+
+def init_globals():
+    global rootd, app, db
+    rootd = os.path.dirname(os.path.abspath(__file__))
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = '{}/sqlite.db'
+    db = SQLAlchemy(app)
+    logger = logging.getLoggerClass()
+
 
 def parse_csv(csv_path: str):
     total_pos = 0.0
@@ -39,11 +48,19 @@ def parse_csv(csv_path: str):
             else:
                 total_neg += pfloat
 
-    print('Positive: {:.2f} | Negative: {:.2f} | Total: {:.2f}'.format(total_pos, total_neg, total_pos + total_neg))
+    logger.info('Positive: {:.2f} | Negative: {:.2f} | Total: {:.2f}'.format(total_pos, total_neg, total_pos + total_neg))
     return all_rows
 
-def main():
+def global_imports():
     global rootd, app, db
+    gimports = ['rootd', 'app', 'db', 'fname']
+
+@click.command()
+@click.option('fname', default=['me.csv'], help='names of files to "proccess"',
+            type=str, multiple=True, nargs=-1)
+def main():
+    global rootd, app, db = init_globals()
+    print('New main test: {filenames}')
     parse_csv(csv_path=rootd + '/' + str(sys.argv[1]))
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,7 @@
+from tkinter import E
+
+
+LOGGING = {
+    
+}
+
